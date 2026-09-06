a1d4d3e64226d9a8510a751a1d247227a06b5d4f
Michael Steven Oliva Robi <msolivar@uqvirtual.edu.co>
2026-09-05 20:05:48 -0500

    Navegacion Angular
    Asi se cargan los componentes en angular en la navegacion

--- ProyectoAngularMaquetadoBootstrap/src/app/app.routes.ts
+++ ProyectoAngularMaquetadoBootstrap/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+// import { LoginComponent } from './componentes/login/login.component';
+// import { RegistroComponent } from './componentes/registro/registro.component';
 import { GestionEventosComponent } from './componentes/gestion-eventos/gestion-eventos.component';
 import { DetalleEventoComponent } from './componentes/detalle-evento/detalle-evento.component';
 
 export const routes: Routes = [
+    { path: '', component: InicioComponent }, //Se carga el componente de inicio al entrar a la aplicación
+//  { path: 'login', component: LoginComponent },
+//  { path: 'registro', component: RegistroComponent },
     { path: "gestion-eventos", component: GestionEventosComponent },
     { path: 'detalle-evento/:id', component: DetalleEventoComponent },
     { path: "**", pathMatch: "full", redirectTo: "" }
