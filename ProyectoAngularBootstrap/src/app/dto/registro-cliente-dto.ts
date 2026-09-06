f02fc8d22323bc80cbfed39718666b76b1549b5f
Michael Steven Oliva Robi <msolivar@uqvirtual.edu.co>
2026-09-05 22:28:34 -0500

    Actualizacion RegistroClienteDTO
    Campos nuevos telefono y email

--- ProyectoAngularBootstrap/src/app/dto/registro-cliente-dto.ts
+++ ProyectoAngularBootstrap/src/app/dto/registro-cliente-dto.ts
@@ -2,10 +2,12 @@ export class RegistroClienteDTO {
     constructor(
     public cedula: string = '',
     public nombre: string = '',
+    public email: string = '',
+    public telefono: string = '',
     public tipo: string = '',
     public password: string = '',
-    public confirmaPassword: string = '',
+    public confirmaPassword: string = '',   
     ) { }
 }
-// Las Clases Dtos RegistroClienteDTO 
+// Las Clases Dtos RegistroClienteDTO
 // se debe crear igual a los Dtos del backend
